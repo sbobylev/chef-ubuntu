fcb2572eca3d180c4729db4cdb0e050e796377b7
Stas <stas.bobylev@gmail.com>
2016-04-22 10:22:02 -0700

    ssd trim support

--- cookbooks/sb-ubuntu-desktop/recipes/system.rb
+++ cookbooks/sb-ubuntu-desktop/recipes/system.rb
@@ -12,3 +12,22 @@ mount '/' do
   action   [:enable]
   only_if 'grep 0 /sys/block/sda/queue/rotational'
 end
+
+cron 'fstrim' do
+  action :create
+  time	:daily
+  command 'fstrim -v /'
+  only_if 'grep 0 /sys/block/sda/queue/rotational'
+end
+
+cookbook_file '/etc/default/grub' do
+  source 'grub'
+  mode '0544'
+  notifies :run, 'execute[update-grub]', :immediately
+  only_if 'grep 0 /sys/block/sda/queue/rotational'
+end
+
+execute 'update-grub' do
+  command 'update-grub'
+  action :nothing
+end
