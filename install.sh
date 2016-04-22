1744ea0d36e02740c82236732b63cb07b829af42
Your Name <you@example.com>
2016-04-22 12:42:24 -0700

    minor change

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ if [ "$EUID" -eq 0 ]; then
     exit
 fi
 
-#curl -L https://packages.chef.io/stable/ubuntu/12.04/chefdk_0.12.0-1_amd64.deb -o /tmp/chefdk_0.12.0-1_amd64.deb
+curl -L https://packages.chef.io/stable/ubuntu/12.04/chefdk_0.12.0-1_amd64.deb -o /tmp/chefdk_0.12.0-1_amd64.deb
 
-#dpkg -i /tmp/chefdk_0.12.0-1_amd64.deb
+dpkg -i /tmp/chefdk_0.12.0-1_amd64.deb
 
 sudo /usr/bin/chef-solo -c solo.rb -j solo.json
 
